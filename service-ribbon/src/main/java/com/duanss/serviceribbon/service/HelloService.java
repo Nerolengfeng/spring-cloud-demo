3ca8c4f11ba997f7b007628baccbf648dcda7edb
duanss <duanss@videon.com>
2018-08-10 16:20:45 +0800

    ribbon 中加入了熔断器hystrix

--- service-ribbon/src/main/java/com/duanss/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/duanss/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.duanss.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,16 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloError")
     public String hello(String name){
         return restTemplate.getForObject("http://HELLO-SERVICE/hello?name="+name,String.class);
     }
+
+    public String helloError(String name){
+        return "oh my god , found a error " + name;
+    }
+
+
 
 
 }
